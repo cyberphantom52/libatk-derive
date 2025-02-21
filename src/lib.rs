0e47a499696d985d14aca60012ce9682bbd3c259
Cyber Phantom <inam123451@gmail.com>
2025-02-21 19:11:10 +0530

    Add Ext trait generation

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 use proc_macro::TokenStream;
-
 use quote::quote;
-use syn::parse::{Parse, ParseStream};
-use syn::punctuated::Punctuated;
-use syn::{parse_macro_input, DeriveInput, Result, Token};
-use syn::{Expr, ExprLit, Lit, MetaNameValue};
+use syn::{
+    parse::{Parse, ParseStream},
+    parse_macro_input,
+    punctuated::Punctuated,
+    AngleBracketedGenericArguments, DeriveInput, Expr, ExprLit, GenericArgument, Ident, ImplItem,
+    ItemImpl, Lit, MetaNameValue, PathArguments, Result, Token, Type, TypePath,
+};
 
 /// Struct for holding the parsed attribute arguments.
 struct Attributes {
@@ -113,4 +115,128 @@ pub fn derive_my_trait(input: TokenStream) -> TokenStream {
     };
 
     TokenStream::from(gen)
+}
+
+#[proc_macro_attribute]
+pub fn command_extension(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as ItemImpl);
+
+    let target_type = &*input.self_ty;
+
+    // Expect target_type looks like Command<Something>
+    let (_, generic_arg_type) = match target_type {
+        Type::Path(TypePath { path, .. }) => {
+            let first_segment = path.segments.first().expect("Expected a path segment");
+            if first_segment.ident != "Command" {
+                return syn::Error::new_spanned(
+                    target_type,
+                    "command_extension only works on impl blocks for Command<T>",
+                )
+                .to_compile_error()
+                .into();
+            }
+            // Extract the single generic argument T from Command<T>.
+            match &first_segment.arguments {
+                PathArguments::AngleBracketed(AngleBracketedGenericArguments { args, .. }) => {
+                    if args.len() != 1 {
+                        return syn::Error::new_spanned(
+                            &first_segment.arguments,
+                            "Expected exactly one generic argument",
+                        )
+                        .to_compile_error()
+                        .into();
+                    }
+                    let generic_arg = args.first().unwrap();
+                    match generic_arg {
+                        GenericArgument::Type(ty) => (first_segment.ident.clone(), ty.clone()),
+                        _ => {
+                            return syn::Error::new_spanned(
+                                generic_arg,
+                                "Expected a type as the generic argument",
+                            )
+                            .to_compile_error()
+                            .into();
+                        }
+                    }
+                }
+                _ => {
+                    return syn::Error::new_spanned(
+                        &first_segment.arguments,
+                        "Expected angle bracketed generic arguments",
+                    )
+                    .to_compile_error()
+                    .into();
+                }
+            }
+        }
+        _ => {
+            return syn::Error::new_spanned(
+                target_type,
+                "command_extension can only be applied to impl blocks for Command<T>",
+            )
+            .to_compile_error()
+            .into();
+        }
+    };
+
+    let inner_type_ident = match generic_arg_type {
+        Type::Path(TypePath { ref path, .. }) => path
+            .segments
+            .last()
+            .expect("Expected at least one segment in the generic type")
+            .ident
+            .clone(),
+        _ => {
+            return syn::Error::new_spanned(
+                generic_arg_type,
+                "Expected a simple type for the generic parameter",
+            )
+            .to_compile_error()
+            .into();
+        }
+    };
+
+    let trait_name_str = format!("{}Ext", inner_type_ident);
+    let trait_ident = Ident::new(&trait_name_str, proc_macro2::Span::call_site());
+
+    let mut trait_methods = Vec::new();
+    let mut impl_methods = Vec::new();
+
+    for item in input.items.iter() {
+        if let ImplItem::Fn(method) = item {
+            let sig = &method.sig;
+            let attrs = &method.attrs;
+            let trait_method = quote! {
+                #(#attrs)*
+                #sig;
+            };
+            trait_methods.push(trait_method);
+
+            let block = &method.block;
+            let impl_method = quote! {
+                #(#attrs)*
+                #sig #block
+            };
+            impl_methods.push(impl_method);
+        }
+    }
+
+    let trait_def = quote! {
+        pub trait #trait_ident {
+            #(#trait_methods)*
+        }
+    };
+
+    let impl_block = quote! {
+        impl #trait_ident for #target_type {
+            #(#impl_methods)*
+        }
+    };
+
+    let output = quote! {
+        #trait_def
+        #impl_block
+    };
+
+    output.into()
 }
